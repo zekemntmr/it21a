0c9a0d2dbba9a57deab1ca44e6001c6c106f9ae9
zekemntmr <zekemntmr@gmail.com>
2025-08-28 08:51:09 +0800

    naa nay sentence

--- hello.js
+++ hello.js
@@ -4,11 +4,13 @@ num1 = 12;
 num2 = 3;
 
 sum = num1 + num2;
-prod = num1 * num2;
 diff = num1 - num2;
+prod = num1 * num2;
 quo = num1 / num2;
 
-console.log(sum);
-console.log(prod);
-console.log(prod);
-console.log(diff);
+console.log("the sum of" + num1 +" and "+ num2 +" is "+ sum);
+console.log("the difference of" + num1 +" and "+ num2 +" is "+ diff);
+console.log("the product of" + num1 +" and "+ num2 +" is "+ prod);
+console.log("the quotient of" + num1 +" and "+ num2 +" is "+ quo);
+
+("the sum of" + num1 +" and "+ num2 +" is "+ sum)
