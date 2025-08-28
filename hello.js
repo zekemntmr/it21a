5e9656dfdf42068120bef6b6fe1526168c7417fc
zekemntmr <zekemntmr@gmail.com>
2025-08-28 08:47:09 +0800

    Addeds subtraction, multiplication and division

--- hello.js
+++ hello.js
@@ -1,8 +1,14 @@
 console.log("Hello World");
 
-num1 = 2;
+num1 = 12;
 num2 = 3;
 
 sum = num1 + num2;
+prod = num1 * num2;
+diff = num1 - num2;
+quo = num1 / num2;
 
 console.log(sum);
+console.log(prod);
+console.log(prod);
+console.log(diff);
