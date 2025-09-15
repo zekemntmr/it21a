a8a81a7f28c34c2cd13ef12df86a0528e1a214bc
zekemntmr <zekemntmr@gmail.com>
2025-09-15 08:34:09 +0800

    added another class and INVOKER

--- hero.js
+++ hero.js
@@ -22,6 +22,21 @@ class Warrior extends Hero{
 const warrior = new Warrior("Thorin", 100, 10);
 warrior.useAbility();
 
+class Wizard extends Hero{
+    constructor(name,health,attack,mana){
+        super(name,health,attack);
+        this.mana = mana;
+
+    }
+    useAbility(){
+        console.log(`${this.name} uses ROCKET LAUNCHER`);
+    }
+
+}
+
+const wizard = new Wizard("Invoker", 100, 5);
+wizard.useAbility();
+
 //const hero = new Hero("Furion", 100, 10);
 
 //console.log(hero.getHealth());
